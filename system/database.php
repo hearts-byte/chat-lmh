d21c20011cf45c4381fea2b1a38cf4965336ea4c
hearts-byte <tlaqywtalt@gmail.com>
2026-08-19 19:56:12 +0300

    Update database configuration for deployment

--- system/database.php
+++ system/database.php
@@ -3,7 +3,7 @@
 define('BOOM_PREFIX', '');
 
 // optional base domain
-define('BOOM_DOMAIN', '');
+define('BOOM_DOMAIN', 'https://chat-lmh-arab.up.railway.app/');
 
 // default redis configuration
 define('REDIS_IP', '127.0.0.1');
@@ -12,16 +12,16 @@ define('REDIS_TIMEOUT', 0.2);
 define('REDIS_PASS', '');
 
 // you can edit these lines to configure new setting for your chat
-define('BOOM_DHOST', '');
-define('BOOM_DUSER', '');
-define('BOOM_DPASS', '');
-define('BOOM_DNAME', '');
+define('BOOM_DHOST', 'mysql.railway.internal');
+define('BOOM_DUSER', 'root');
+define('BOOM_DPASS', 'sZLChOUsoBCzfSYACSRmeHAnLHgaPGaj');
+define('BOOM_DNAME', 'railway');
 
 // base system main path do not modify
 define('BOOM_PATH', dirname(__DIR__));
 
 // do not modify those variables
 define('BOOM_CRYPT', '');
-define('BOOM_INSTALL', 0);
+define('BOOM_INSTALL', 1);
 define('BOOM', 1);
 ?>
