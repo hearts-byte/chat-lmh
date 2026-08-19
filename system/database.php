a41314c1b5a87c9dcb4a0ddfc48f7a1c190be708
hearts-byte <tlaqywtalt@gmail.com>
2026-08-19 19:37:20 +0300

    Update database.php

--- system/database.php
+++ system/database.php
@@ -12,16 +12,16 @@ define('REDIS_TIMEOUT', 0.2);
 define('REDIS_PASS', '');
 
 // you can edit these lines to configure new setting for your chat
-define('BOOM_DHOST', 'mysql-qnf9.railway.internal');
-define('BOOM_DUSER', 'root');
-define('BOOM_DPASS', 'xsNOTIIvKkShhYMAFWpCwMKkmMvkKqKH');
-define('BOOM_DNAME', 'railway');
+define('BOOM_DHOST', '');
+define('BOOM_DUSER', '');
+define('BOOM_DPASS', '');
+define('BOOM_DNAME', '');
 
 // base system main path do not modify
 define('BOOM_PATH', dirname(__DIR__));
 
 // do not modify those variables
 define('BOOM_CRYPT', '');
-define('BOOM_INSTALL', 1);
+define('BOOM_INSTALL', 0);
 define('BOOM', 1);
 ?>
